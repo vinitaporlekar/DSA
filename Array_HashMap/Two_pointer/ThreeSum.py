a57154eeaba6976ae5700fa15819825148ffc4c7
vinitaporlekar <vinitaporlekar2@gmail.com>
2026-05-13 18:07:22 -0400

    solved leetcode problem 15

---
+++ Array_HashMap/Two_pointer/ThreeSum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        res =[]
+        nums.sort()
+
+        for i , a in enumerate(nums):
+            if i> 0 and a== nums[i-1]:
+                continue
+            
+            l, r= i+1, len(nums) - 1
+            while l < r  :
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r=  r-1
+                elif threeSum < 0:
+                    l= l+1
+                else:
+                    res.append([a, nums[l],nums[r]])
+                    l= l+1
+                    while nums[l] == nums[l - 1] and l< r:
+                        l=l+1
+                    
+        return res
+
+                
+
+nums = [-1,0,1,2,-1,-4]
+sol = Solution()
+print(sol.threeSum(nums))
